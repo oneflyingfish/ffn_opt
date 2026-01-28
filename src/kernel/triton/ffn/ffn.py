a9daa9006caf1754e0369858e9025de6a11284a2
patrickhashimoto <patrickmupersonal@outlook.com>
2026-01-28 03:46:28 +0000

    remove third_party

---
+++ src/kernel/triton/ffn/ffn.py
@@ -0,0 +1,7 @@
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def ffn_trtion():
+    pass
--- test_ffn.py
+++
@@ -1,5 +0,0 @@
-
-import third_party
-from src.python.ffn.ffn import generate
-
-generate()
